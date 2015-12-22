791ef7f37ef69d39dd9214a2061f295665a6f207
Vilson Vieira <vilson@void.cc>
2015-12-22 15:14:15 -0200

    List pango include dir

--- lib/dependencies.sh
+++ lib/dependencies.sh
@@ -26,6 +26,9 @@ install_node_modules() {
     echo "pkg-config cairo"
     pkg-config --libs cairo
     pkg-config --cflags cairo
+
+    echo "Listing /app/vendor/pango/include/pango-1.0..."
+    ls /app/vendor/pango/include/pango-1.0
     npm install --unsafe-perm --userconfig $build_dir/.npmrc 2>&1
   else
     echo "Skipping (no package.json)"
